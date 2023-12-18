7bc163c2b79ac33d88c5ff561cc58603fe79677d
GustavoMCorrea <gustavomotacorrea@gmail.com>
2023-12-18 13:55:38 -0300

    Adicionando controle de velocidade no personagem

--- MundoDaConcentracao/Assets/Scripts/PlayerController.cs
+++ MundoDaConcentracao/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRigidbody2D;
-    private Animation playerAnimator;
+    private Animator playerAnimator;
     public float playerSpeed;
+    private float playerInitialSpeed;
+    public float playerRunSpeed;
     private Vector2 playerDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
-        playerAnimator = GetComponent<Animation>();
+        playerAnimator = GetComponent<Animator>();
+
+        playerInitialSpeed = playerSpeed;
     }
 
     // Update is called once per frame
@@ -23,8 +27,16 @@ public class PlayerController : MonoBehaviour
 
         if (playerDirection.sqrMagnitude > 0)
         {
-
+            playerAnimator.SetInteger("Movimento", 1);
+        } 
+        else
+        {
+            playerAnimator.SetInteger("Movimento", 0);
         }
+
+        Flip();
+
+        PlayerRun();
 
     }
 
@@ -34,4 +46,30 @@ public class PlayerController : MonoBehaviour
         playerRigidbody2D.MovePosition(playerRigidbody2D.position + playerDirection * playerSpeed * Time.fixedDeltaTime);
         
     }
+    //Muda a dureção da animação do personagem
+    void Flip()
+    {
+        if (playerDirection.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+
+        }
+        else if (playerDirection.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+    }
+
+    void PlayerRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            playerSpeed = playerRunSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            playerSpeed = playerInitialSpeed;
+        }
+    }
+
 }
